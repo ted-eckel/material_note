21cf3d8aafaab3d1ad05e136e38932379d2fdeee
Ted Eckel <teckel1029@gmail.com>
2016-08-31 13:11:36 -0400

    update UsersController to give new users a note and notebook on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in(@user)
+      @user.notebooks.create(title: "Your First Notebook!", description: nil).notes.create(title: "Your First Note!", body: "<div>This is a note. You can find notes within their notebooks. In order to create a note, click the floating action button on the bottom-right, and give your note a title and a body. Notes can be edited by clicking them in the sidebar menu, in their respective notebook. In order to create a notebook, hover over the floating action button on the bottom-right, and click the notebook icon. Then give your notebook a title!</div><div><br></div><div><br></div><div>Check out my: </div><div><a href=\"https://github.com/ted-eckel\"><span style=\"color: rgb(17, 85, 204); font-family: arial, sans-serif; font-size: small;\">GitHub</span></a><span style=\"color: rgb(34, 34, 34); font-family: arial, sans-serif; font-size: small;\">&nbsp;-&nbsp;</span><a href=\"https://www.linkedin.com/in/tedjeckel\"><span style=\"color: rgb(17, 85, 204); font-family: arial, sans-serif; font-size: small;\">LinkedIn</span></a><span style=\"color: rgb(34, 34, 34); font-family: arial, sans-serif; font-size: small;\">&nbsp;-&nbsp;</span><a href=\"http://tedeckel.me/\"><span style=\"color: rgb(17, 85, 204); font-family: arial, sans-serif; font-size: small;\">Portfolio</span></a></div>")
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
